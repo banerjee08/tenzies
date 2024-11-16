bf8ce20aca62beb843e59b63e5f07097e71d947d
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2024-11-16 23:21:42 +0530

    html and css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-
 import './App.css'
 
 function App() {
   
-
   return (
     <>
+      <div className="container">
+        <div className='game-container'>
+          <div className='intro'>
+            <h1 className='title'>TENZIES</h1>
+            <p className='desc'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+          </div>
+          <div className="dice-container">
+            <h3 className='die'>1</h3>
+            <h3 className='die'>1</h3>
+            <h3 className='die'>1</h3>
+            <h3 className='die'>1</h3>
+            <h3 className='die'>1</h3>
+            <h3 className='die'>1</h3>
+            <h3 className='die'>1</h3>
+            <h3 className='die'>1</h3>
+            <h3 className='die'>1</h3>
+            <h3 className='die'>1</h3>
+          </div>
+          <button className='button'>Roll</button>
+        </div>
+      </div>
     </>
   )
 }
