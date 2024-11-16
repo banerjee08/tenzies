8c3acaea20b7e42e3074cd38dc18a8fdb21b39df
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2024-11-16 23:49:33 +0530

    Generating new set of Dice when clicking Roll button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import './App.css'
+import React from 'react'
+import Die from './components/Die'
+import { nanoid } from 'nanoid'
 
 function App() {
+
+  function rollDice(){
+    setDice(allNewDice())
+  }
+  // function for creating a new deck of dice
+  function allNewDice(){
+    let newDice = []
+    for(let i = 0; i < 10; i++){
+      let newDie = Math.floor(Math.random() * 6) + 1
+      newDice.push(newDie)
+    }
+    return newDice
+  }
+
+  const [dice, setDice] = React.useState(allNewDice())
+
+  // Rendering die elements
+  const dieElements = dice.map(die => {
+    return <Die key={nanoid()} value={die} />
+  })
+  // console.log(dice)
   
   return (
     <>
@@ -11,18 +35,9 @@ function App() {
             <p className='desc'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           </div>
           <div className="dice-container">
-            <h3 className='die'>1</h3>
-            <h3 className='die'>1</h3>
-            <h3 className='die'>1</h3>
-            <h3 className='die'>1</h3>
-            <h3 className='die'>1</h3>
-            <h3 className='die'>1</h3>
-            <h3 className='die'>1</h3>
-            <h3 className='die'>1</h3>
-            <h3 className='die'>1</h3>
-            <h3 className='die'>1</h3>
+            {dieElements}
           </div>
-          <button className='button'>Roll</button>
+          <button className='button' onClick={rollDice}>Roll</button>
         </div>
       </div>
     </>
---
+++ src/components/Die.jsx
@@ -0,0 +1,8 @@
+function Die(props){
+    // console.log(props)
+    return(
+        <h3 className='die'>{props.value}</h3>
+    )
+}
+
+export default Die
